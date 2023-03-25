da3b08b3683637f44e10032323f66fc53d8ee937
Trong_Nguyen_Van <trong.nguyenbk1702@hcmut.edu.vn>
2023-03-25 17:08:05 +0700

    Reorder files. Add homepage(not finished)

---
+++ UI/PHP/homepage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Home</title>
+	<link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+	<div class="background">
+	   <img src="../UI/Imgs/background.jpg">
+	</div>
+	<div class="logo">
+		<img src="UI/Imgs/logo.png">
+	</div>
+	<div class="text">
+		<p>TRE VIET</p>
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">FEATURES</a></li>
+			<li><a href="#">ABOUT</a></li>
+			<li><a href="#">CONTACT</a></li>
+		</ul>
+	</div>
+	<div class="button">
+		<a href="#">
+			<div>
+				<p>CLICK TO CONTROL YOUR HOME</p>
+			</div>
+		</a>
+	</div>
+	<div class="footer">
+		<p>A PRODUCT MADE BY TREVIET</p>
+	</div>
+</body>
+</html>
